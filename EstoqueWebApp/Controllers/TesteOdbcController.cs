f265172c68db22838783077861eae1f481b7ffbb
Ana <Ana@DESKTOP-AGICS9D>
2021-02-27 16:52:43 -0300

    estoque

---
+++ EstoqueWebApp/Controllers/TesteOdbcController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstoqueWebApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class TesteOdbcController : ControllerBase
+    {
+        // GET: <TesteOdbcController>
+        [HttpGet]
+        public object Get()
+        {
+            return new { teste = TesteConexaoOdbc.Teste() };
+        }
+    }
+}
---
+++ EstoqueWebApp/TesteConexaoOdbc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Threading.Tasks;
+namespace EstoqueWebApp
+{
+    public class TesteConexaoOdbc
+    {
+        public static object Teste()
+        {
+            using var connection = new OdbcConnection("Driver={ODBC Driver 17 for SQL Server};Server=.\\SQLEXPRESS;Database=estoque;Uid=admin;Pwd=senha;");
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = "select top 1 txt_descricao from produto";
+            return command.ExecuteScalar();
+        }
+    }
+}
